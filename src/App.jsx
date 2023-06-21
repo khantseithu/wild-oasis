47ddf92e7c061ca893c85c36c439cf7960e8e0d5
Khant Sithu <khantseithuu@gmail.com>
2023-06-21 17:20:12 +0000

    feat: Build booking detail page

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,18 @@
-import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
-import GlobalStyles from "./styles/GlobalStyles";
-import Dashboard from "./pages/Dashboard";
-import PageNotFound from "./pages/PageNotFound";
-import Account from "./pages/Account";
-import Bookings from "./pages/Bookings";
-import Cabins from "./pages/Cabins";
-import Login from "./pages/Login";
-import Settings from "./pages/Settings";
-import Users from "./pages/Users";
-import AppLayout from "./ui/AppLayout";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
-import { Toaster } from "react-hot-toast";
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
+import GlobalStyles from './styles/GlobalStyles';
+import Dashboard from './pages/Dashboard';
+import PageNotFound from './pages/PageNotFound';
+import Account from './pages/Account';
+import Bookings from './pages/Bookings';
+import Cabins from './pages/Cabins';
+import Login from './pages/Login';
+import Settings from './pages/Settings';
+import Users from './pages/Users';
+import AppLayout from './ui/AppLayout';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+import { Toaster } from 'react-hot-toast';
+import Booking from './pages/Booking';
 
 const queryClient = new QueryClient({
   defaultOptions: {
@@ -32,6 +33,7 @@ const App = () => {
             <Route path="/dashboard" element={<Dashboard />} />
             <Route path="/cabins" element={<Cabins />} />
             <Route path="/bookings" element={<Bookings />} />
+            <Route path="/bookings/:id" element={<Booking />} />
             <Route path="/account" element={<Account />} />
             <Route path="/settings" element={<Settings />} />
             <Route path="/users" element={<Users />} />
@@ -44,7 +46,7 @@ const App = () => {
       <Toaster
         position="top-center"
         gutter={12}
-        containerStyle={{ margin: "8px" }}
+        containerStyle={{ margin: '8px' }}
         toastOptions={{
           success: {
             duration: 3000,
@@ -53,11 +55,11 @@ const App = () => {
             duration: 5000,
           },
           style: {
-            fontSize: "16px",
-            maxWidth: "500px",
-            padding: "16px 24px",
-            backgroundColor: "var(--color-grey-0)",
-            color: "var(--color-grey-700)",
+            fontSize: '16px',
+            maxWidth: '500px',
+            padding: '16px 24px',
+            backgroundColor: 'var(--color-grey-0)',
+            color: 'var(--color-grey-700)',
           },
         }}
       />
---
+++ src/features/bookings/useBooking.js
@@ -0,0 +1,19 @@
+import { useQuery } from '@tanstack/react-query';
+import { useParams } from 'react-router-dom';
+import { getBooking } from '../../services/apiBookings';
+
+export function useBooking() {
+  const { id: bookingId } = useParams();
+  console.log(bookingId);
+
+  const {
+    isLoading,
+    data: booking,
+    error,
+  } = useQuery({
+    queryKey: ['booking', bookingId],
+    queryFn: () => getBooking(bookingId),
+  });
+
+  return { isLoading, error, booking };
+}
