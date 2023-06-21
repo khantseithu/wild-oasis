69fe189e1ede4948ae8308d986f6592c3151dad0
Khant Sithu <khantseithuu@gmail.com>
2023-06-21 16:23:00 +0000

    env: gitpod

--- src/features/bookings/useBookings.js
+++ src/features/bookings/useBookings.js
@@ -1,30 +1,33 @@
-import { useQuery } from "@tanstack/react-query";
-import { getBookings } from "../../services/apiBookings";
-import { useSearchParams } from "react-router-dom";
+import { useQuery } from '@tanstack/react-query';
+import { getBookings } from '../../services/apiBookings';
+import { useSearchParams } from 'react-router-dom';
 
 export function useBookings() {
-	const [searchParams] = useSearchParams();
+  const [searchParams] = useSearchParams();
 
-	// FILTER
-	const filterValue = searchParams.get("status");
-	const filter =
-		!filterValue || filterValue === "all"
-			? null
-			: { field: "status", value: filterValue };
+  // FILTER
+  const filterValue = searchParams.get('status');
+  const filter =
+    !filterValue || filterValue === 'all'
+      ? null
+      : { field: 'status', value: filterValue };
 
-	// SORT
-	const sortByRaw = searchParams.get("sortBy") || "startDate-desc";
-	const [field, direction] = sortByRaw.split("-");
-	const sortBy = { field, direction };
+  // SORT
+  const sortByRaw = searchParams.get('sortBy') || 'startDate-desc';
+  const [field, direction] = sortByRaw.split('-');
+  const sortBy = { field, direction };
 
-	const {
-		data: bookings,
-		isLoading,
-		error,
-	} = useQuery({
-		queryKey: ["bookings", filter, sortBy],
-		queryFn: () => getBookings({ filter, sortBy }),
-	});
+  // PAGINATION
+  const page = !searchParams.get('page') ? 1 : Number(searchParams.get('page'));
 
-	return { bookings, isLoading, error };
+  const {
+    data: { data: bookings, count } = {},
+    isLoading,
+    error,
+  } = useQuery({
+    queryKey: ['bookings', filter, sortBy, page],
+    queryFn: () => getBookings({ filter, sortBy, page: page }),
+  });
+
+  return { bookings, isLoading, error, count };
 }
