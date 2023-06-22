5cc8238269ff70ef65e02ae3b6757bd1c6c76942
Khant Sithu <khantseithuu@gmail.com>
2023-06-22 17:17:24 +0000

    feat: Authentication and protect routes

---
+++ src/features/authentication/useLogin.js
@@ -0,0 +1,25 @@
+import { useMutation, useQueryClient } from '@tanstack/react-query';
+import { login as loginApi } from '../../services/apiAuth';
+import { useNavigate } from 'react-router-dom';
+import { toast } from 'react-hot-toast';
+
+function useLogin() {
+  const navigate = useNavigate();
+  const queryClient = useQueryClient();
+  const { mutate: login, isLoading } = useMutation({
+    mutationFn: ({ email, password }) => loginApi({ email, password }),
+    onSuccess: (user) => {
+      queryClient.setQueryData(['user'], user.user);
+      navigate('/dashboard');
+      console.log(user);
+    },
+    onError: (error) => {
+      console.log('ERROR', error);
+      toast.error('Provied email or password is incorrect!');
+    },
+  });
+
+  return { login, isLoading };
+}
+
+export default useLogin;
